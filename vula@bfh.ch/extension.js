7adf88c775a60c1256edbbe9d55eac2e60cdd67f
Mühlemann Sarah Fatima <sarahfatima.muehlemann@students.bfh.ch>
2021-12-23 18:28:15 +0100

    implement repair, status, verify my-vk commands Co-authored-by: Elia Bieri <info@eliabieri.dev>

--- vula@bfh.ch/extension.js
+++ vula@bfh.ch/extension.js
@@ -19,6 +19,7 @@ const Main = imports.ui.main;
 
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const MessageTray = imports.ui.messageTray;
 
 const St = imports.gi.St;
 
@@ -36,82 +37,93 @@ const Vula_Indicator = new Lang.Class({
     Name: 'Vula.indicator',
     Extends: PanelMenu.Button,
 
-       _init: function(){
-            const vulaPath = '/usr/bin/vula';
-            this.parent(0.0);
-            this._icon = new St.Icon({style_class: 'system-status-icon',});
-            this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA.svg`);
-            this.add_actor(this._icon);
+    _init: function () {
+        const vulaPath = '/usr/local/bin/vula';
+        this.parent(0.0);
+        this._icon = new St.Icon({ style_class: 'system-status-icon', });
+        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA.svg`);
+        this.add_actor(this._icon);
 
-            // Toggle button start/stop Vula
-            let switchmenuitem = new PopupMenu.PopupSwitchMenuItem('Start Vula', false);
-            switchmenuitem.actor.connect('toggled', Lang.bind(this, function(object, value){
-                try {
-                    if(value) {
-                        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA_ACTIVE.svg`);
-                        Util.spawn([vulaPath, 'start']);
-                        Main.notify('Vula Notification', 'Vula started');
-                    } else {
-                        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA.svg`);
-                        Util.spawn(['/bin/systemctl', 'stop', 'vula.slice']);
-                        Main.notify('Vula Notification', 'Vula stopped');
-                    }
+        // Toggle button start/stop Vula
+        let switchmenuitem = new PopupMenu.PopupSwitchMenuItem('Start Vula', false);
+        switchmenuitem.actor.connect('toggled', Lang.bind(this, function (object, value) {
+            try {
+                if (value) {
+                    this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA_ACTIVE.svg`);
+                    Util.spawn([vulaPath, 'start']);
+                    Main.notify('Vula Notification', 'Vula started');
+                } else {
+                    this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/VULA.svg`);
+                    Util.spawn(['/bin/systemctl', 'stop', 'vula.slice']);
+                    Main.notify('Vula Notification', 'Vula stopped');
                 }
-                catch (error) {
-                    Main.notify('Vula Notification', `An error occured while starting Vula: ${error}`);
-                    return;
-                }
-            }));
+            }
+            catch (error) {
+                Main.notify('Vula Notification', `An error occured while starting Vula: ${error}`);
+                return;
+            }
+        }));
 
-            // Vula Repair
-            let vulaRepairMenuItem = new PopupMenu.PopupMenuItem("Repair Vula", {});
-            vulaRepairMenuItem.actor.connect('activate', Lang.bind(this, function(){
-                try {
-                    Util.spawn([vulaPath, 'repair']);
-		            Main.notify('Vula Notification', 'Vula repaired');
-                }
-                catch (error) {
-                    Main.notify('Vula Notification', `An error occured while repairing Vula: ${error}`);
-                    return;
-                }
-		    }));
+        // Vula Repair
+        let vulaRepairMenuItem = new PopupMenu.PopupMenuItem("Repair Vula", {});
+        vulaRepairMenuItem.actor.connect('activate', Lang.bind(this, function () {
+            try {
+                Util.spawn([vulaPath, ' repair']);
+                Main.notify('Vula Notification', 'Vula repaired');
+            }
+            catch (error) {
+                Main.notify('Vula Notification', `An error occured while repairing Vula: ${error}`);
+                return;
+            }
+        }));
+
+        // Vula Status Button
+        let vulaStatusItem = new PopupMenu.PopupMenuItem("Get Vula status", {});
+        vulaStatusItem.actor.connect('activate', Lang.bind(this, function () {
+            try {
+                let processOutput = GLib.spawn_command_line_sync(vulaPath + " status")[1].toString();
+                Main.notify('Vula Notification', processOutput);
+            }
+            catch (error) {
+                Main.notify('Vula Notification', `An error occured while getting Vula status: ${error}`);
+                return;
+            }
+        }));
 
 
-            // Vula Status
-            let vulaStatusItem = new PopupMenu.PopupMenuItem("Get Vula Status", {});
-            vulaStatusItem.actor.connect('activate', Lang.bind(this, function(){
-                try {
-                    let stuff = GLib.spawn_command_line_sync(vulaPath + " status")[1].toString();
-                    log ('stuff: ' + stuff);
-                    Main.notify('Vula Notification', stuff);
-                }
-                catch (error) {
-                    Main.notify('Vula Notification', `An error occured while getting Vula status: ${error}`);
-                    return;
-                }
-		    }));
-            
+        // Vula VK Button
+        let getVkItem = new PopupMenu.PopupMenuItem("Get verification key", {});
+        getVkItem.actor.connect('activate', Lang.bind(this, function () {
+            try {
+                let processOutput = GLib.spawn_command_line_sync(vulaPath + " verify my-vk")[1].toString();
+                Main.notify('Vula Notification', processOutput);
+            }
+            catch (error) {
+                Main.notify('Vula Notification', `An error occured while getting Vula status: ${error}`);
+                return;
+            }
+        }));
 
-            // add Items to menu
-            this.menu.addMenuItem(switchmenuitem);
-            this.menu.addMenuItem(vulaRepairMenuItem);
-            this.menu.addMenuItem(vulaStatusItem);
-
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem);
-       }
- });
+        // Add Items to menu
+        this.menu.addMenuItem(switchmenuitem);
+        this.menu.addMenuItem(vulaRepairMenuItem);
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem);
+        this.menu.addMenuItem(vulaStatusItem);
+        this.menu.addMenuItem(getVkItem);
+    }
+});
 
 function init() {
-    log ('Vula extension initalized');
+    log('Vula extension initalized');
 };
 
 function enable() {
-     log ('Vula extension enabled');
-     let _indicator =  new Vula_Indicator();
-     Main.panel._addToPanelBox('Vula', _indicator, 1, Main.panel._rightBox);
+    log('Vula extension enabled');
+    let _indicator = new Vula_Indicator();
+    Main.panel._addToPanelBox('Vula', _indicator, 1, Main.panel._rightBox);
 };
 
-function disable(){
-     log ('Vula extension disabled');
-     _indicator.destroy();
+function disable() {
+    log('Vula extension disabled');
+    _indicator.destroy();
 };
