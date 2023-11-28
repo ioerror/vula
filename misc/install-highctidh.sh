646a076996db857b517c72bd9f156d7a87526c06
Vula Author 1 <git@vula.link>
2023-11-28 17:45:46 +0100

    Prepare for releasing v0.2.2023112800
    This commit prepares vula for releasing version v0.2.2023112800:
    - Update `misc/install-highctidh.sh` to use highctidh v1.0.2023112800
    -- This ensures that podman builds use the same version of highctidh as
       in the Pipile
    - Update Pipfile to use highctidh v1.0.2023112800
    - Update vula's version to be v0.2.2023112800
    - Update `.woodpecker.yml` to call `make wheel` rather than `make
      pypi-build`
    
    This commit is additionally in a branch to begin a new tradition of
    staging releases in a branch and then using a merge commit on main to
    complete the release process.

--- misc/install-highctidh.sh
+++ misc/install-highctidh.sh
@@ -6,6 +6,7 @@ set -e
 # pip3 install git+https://codeberg.org/vula/highctidh/
 git clone https://codeberg.org/vula/highctidh/
 cd highctidh;
+git checkout v1.0.2023112800
 VERSION=`cat VERSION`;
 time make -f Makefile.packages deb
 ls -al *
