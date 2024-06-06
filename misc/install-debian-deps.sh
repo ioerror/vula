252d4d2a6f1075893347332c3d954513cf7e7124
Leif Ryge <leif@synthesize.us>
2024-06-06 17:25:59 +0200

    (WIP) add ubuntu 24.04 to Makefile
    this doesn't work yet, but gets us closer to having a podman container of it at
    least.
    
    running "make dist=noble shell" will currently fail when trying to build a
    highctidh deb using stdeb.
    
    this commit breaks the ability to build debs or rpms from debian containers,
    because it removes stdeb from install-debian-deps.sh
    
    this WIP commit also removes vula_libnss from the debian containers for now,
    because it also requires stdeb.
    
    "make rpm" in the podman directory *might* actually still work, since it uses
    stdeb on fedora34 (which is also EOL...) but I have not tested that.

--- misc/install-debian-deps.sh
+++ misc/install-debian-deps.sh
@@ -22,12 +22,9 @@ dist="$(cat /etc/os-release|grep VERSION_CODENAME|cut -f 2 -d=)"
 echo "Installing on '$dist'"
 
 # FIXME: some of these probably aren't needed anymore
-highcitdh_deps="dh-python python3-build python3-setuptools python3-stdeb python3-venv python3-wheel python3-pip python3-all-dev flit fakeroot gcc clang git make"
+highcitdh_deps="dh-python python3-build python3-setuptools python3-venv python3-wheel python3-pip python3-all-dev flit fakeroot gcc clang git make"
 
-vula_deps="wireguard-tools python3-yaml python3-click python3-nacl python3-schema python3-pip python3-pathtools make gcc python3-pydbus python3-pyroute2 python3-pytest-runner python3-pytest python3-toml python3-py python3-packaging python3-pluggy python3-hkdf python3-ifaddr python3-cryptography python3-dbus python3-pygments python3-systemd python3-qrcode python3-all fakeroot build-essential dh-python debhelper python3-dev python3-zeroconf python3-all-dev python3-build python3-babel python3-tk python3-sphinx python3-xlib python3-pillow gir1.2-ayatanaappindicator3-0.1 python3-pyaudio python3-stdeb"
-
-# just for pipx installing stdeb from git, so we can pypi-install vula_libnss
-vula_deps="$vula_deps git"
+vula_deps="wireguard-tools python3-yaml python3-click python3-nacl python3-schema python3-pip python3-pathtools make gcc python3-pydbus python3-pyroute2 python3-pytest-runner python3-pytest python3-toml python3-py python3-packaging python3-pluggy python3-hkdf python3-ifaddr python3-cryptography python3-dbus python3-pygments python3-systemd python3-qrcode python3-all fakeroot build-essential dh-python debhelper python3-dev python3-zeroconf python3-all-dev python3-build python3-babel python3-tk python3-sphinx python3-xlib python3-pillow gir1.2-ayatanaappindicator3-0.1 python3-pyaudio"
 
 click_man_deps="python3-click-man click-man"
 
@@ -53,16 +50,6 @@ if [ "$dist" = "bullseye" ] || [ "$dist" = "focal" ] || [ "$dist" = "buster" ]; 
 else
     apt install -y --no-install-recommends pipx
 fi
-
-# use this unmerged PR version of stdeb https://github.com/astraw/stdeb/pull/195
-pipx ensurepath
-export PATH="~/.local/bin:$PATH"
-pipx install git+https://github.com/jcapona/stdeb@63dea27461d # PR 195
-pipx install cython
-
-# workaround bug where pipx install of stdeb doesn't include requests in venv...
-# https://github.com/pypa/pipx/issues/233#issuecomment-540049239
-~/.local/pipx/venvs/stdeb/bin/python3 -m pip install requests
 
 if [ "$dist" = "buster" ]; then
     # buster needs a newer zeroconf
@@ -91,10 +78,14 @@ fi
 #pypi-install highctidh
 # for latest version:
 # pip3 install git+https://codeberg.org/vula/highctidh/
+
+# FIXME: we need to make vula_libnss build a deb without stdeb
 # pip install vula_libnss
+
+
 # for latest version:
-git clone --recursive https://codeberg.org/vula/vula_libnss
-cd vula_libnss && make deb && dpkg -i deb_dist/*.deb
+# git clone --recursive https://codeberg.org/vula/vula_libnss
+# cd vula_libnss && make deb && dpkg -i deb_dist/*.deb
 
 # python3-pystray is only available in Debian Unstable so far
 #DEB_BUILD_OPTIONS=nocheck pypi-install pystray
